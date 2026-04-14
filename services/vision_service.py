2b3337c1ea28fb950b0e21ec3c1bdcc6866bce4f
Vinish Vivek <vinishvivek@gmail.com>
2026-04-15 02:20:37 +0530

    - VisionService to call LLM over image

---
+++ services/vision_service.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+from PIL import Image
+
+from clients.openai_client import OpenAIClientFactory
+from config.settings import settings
+from services.image_encoder_service import ImageEncoderService
+
+class VisionService:
+    def __init__(self) -> None:
+        self._client = OpenAIClientFactory.create()
+
+    def query_client(
+        self,
+        image_to_llm: str | Image.Image,
+        prompt: str,
+        max_tokens: int | None = None,
+        ) -> str:
+        base64_encoded_image = ImageEncoderService.encode_image_to_base64(image_to_llm)
+
+        mime_type = "image/jpeg"
+        if isinstance(image_to_llm, str):
+            mime_type = ImageEncoderService.get_mime_type(image_to_llm)
+
+        response = self._client.chat.completions.create(
+            model=settings.vision_model,
+            max_tokens=max_tokens,
+            messages=[
+                {
+                    "role": "user",
+                    "content":[
+                        {
+                            "type": "text",
+                            "text":prompt
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:{mime_type};base64,{base64_encoded_image}"
+                            }
+                        }
+                    ]
+                }
+            ],
+        )
+
+        return response.choices[0].message.content
+
+
